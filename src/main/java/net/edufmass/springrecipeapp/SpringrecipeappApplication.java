e2b508ea9cda41b182aa619e8899d0105d899624
edufmass <git@edufmass.ar>
2023-10-19 21:03:38 -0300

    initial commit

---
+++ src/main/java/net/edufmass/springrecipeapp/SpringrecipeappApplication.java
@@ -0,0 +1,13 @@
+package net.edufmass.springrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrecipeappApplication.class, args);
+	}
+
+}
