6d7f6f97b0d4504f83a81de0dea80abf391fd1d3
edufmass <git@edufmass.ar>
2024-02-29 15:20:56 -0300

    Added OneToMany relationship Recipe-Ingredient

--- src/main/java/net/edufmass/springrecipeapp/domain/Recipe.java
+++ src/main/java/net/edufmass/springrecipeapp/domain/Recipe.java
@@ -2,6 +2,8 @@ package net.edufmass.springrecipeapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 public class Recipe {
 
@@ -18,6 +20,9 @@ public class Recipe {
     private String directions;
     // todo add
     // private Difficulty difficulty;
+
+    @OneToMany(cascade =  CascadeType.ALL, mappedBy = "recipe") // property of child class
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -88,6 +93,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {
