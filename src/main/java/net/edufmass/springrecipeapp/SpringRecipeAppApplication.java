d69b274fe9ec09b2f7117961c139a7bcc464574a
edufmass <git@edufmass.ar>
2024-02-29 21:29:12 -0300

    app renamed

--- src/main/java/net/edufmass/springrecipeapp/SpringRecipeAppApplication.java
+++ src/main/java/net/edufmass/springrecipeapp/SpringRecipeAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringrecipeappApplication {
+public class SpringRecipeAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringrecipeappApplication.class, args);
+		SpringApplication.run(SpringRecipeAppApplication.class, args);
 	}
 
 }
