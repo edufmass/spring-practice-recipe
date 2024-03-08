99ec408d3c18cb31bba516236d23737ede1259d7
edufmass <git@edufmass.ar>
2024-03-07 21:51:42 -0300

    added recipes list to index

--- src/main/java/net/edufmass/springrecipeapp/controllers/IndexController.java
+++ src/main/java/net/edufmass/springrecipeapp/controllers/IndexController.java
@@ -1,10 +1,8 @@
 package net.edufmass.springrecipeapp.controllers;
 
-import net.edufmass.springrecipeapp.domain.Category;
-import net.edufmass.springrecipeapp.domain.UnitOfMeasure;
-import net.edufmass.springrecipeapp.repositories.CategoryRepository;
-import net.edufmass.springrecipeapp.repositories.UnitOfMeasureRepository;
+import net.edufmass.springrecipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,23 +10,15 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
-
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
-
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 
--- src/main/java/net/edufmass/springrecipeapp/services/RecipeServiceImpl.java
+++ src/main/java/net/edufmass/springrecipeapp/services/RecipeServiceImpl.java
@@ -2,10 +2,12 @@ package net.edufmass.springrecipeapp.services;
 
 import net.edufmass.springrecipeapp.domain.Recipe;
 import net.edufmass.springrecipeapp.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
