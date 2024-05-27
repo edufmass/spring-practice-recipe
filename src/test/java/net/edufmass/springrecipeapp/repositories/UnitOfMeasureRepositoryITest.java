4cf80c26d3e50b0c228942ee75fe2eac77438b89
edufmass <git@edufmass.ar>
2024-05-27 14:58:00 -0300

    UoM Repository integration test

---
+++ src/test/java/net/edufmass/springrecipeapp/repositories/UnitOfMeasureRepositoryITest.java
@@ -0,0 +1,38 @@
+package net.edufmass.springrecipeapp.repositories;
+
+import net.edufmass.springrecipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@RunWith(SpringRunner.class) JUnit4
+@DataJpaTest
+class UnitOfMeasureRepositoryITest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
