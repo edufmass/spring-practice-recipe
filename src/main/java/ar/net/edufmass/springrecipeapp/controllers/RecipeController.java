5be288406b8f5ec3b33af4381066c67177eacc57
edufmass <git@edufmass.ar>
2024-06-21 21:49:19 -0300

    Delete recipe

--- src/main/java/ar/net/edufmass/springrecipeapp/controllers/RecipeController.java
+++ src/main/java/ar/net/edufmass/springrecipeapp/controllers/RecipeController.java
@@ -1,12 +1,13 @@
 package ar.net.edufmass.springrecipeapp.controllers;
 
 import ar.net.edufmass.springrecipeapp.commands.RecipeCommand;
-import ar.net.edufmass.springrecipeapp.domain.Recipe;
 import ar.net.edufmass.springrecipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -16,12 +17,14 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("/recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
@@ -29,10 +32,10 @@ public class RecipeController {
         return "recipe/recipeForm";
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
-        Recipe oneRecipe = recipeService.findById(Long.valueOf(id));
-        model.addAttribute("recipe", oneRecipe);
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeForm";
     }
 
@@ -41,6 +44,16 @@ public class RecipeController {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id){
+
+        log.debug("Deleting id: " + id);
+
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 
 }
--- src/test/java/ar/net/edufmass/springrecipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/ar/net/edufmass/springrecipeapp/services/RecipeServiceImplTest.java
@@ -1,5 +1,6 @@
 package ar.net.edufmass.springrecipeapp.services;
 
+import ar.net.edufmass.springrecipeapp.commands.RecipeCommand;
 import ar.net.edufmass.springrecipeapp.converters.RecipeCommandToRecipe;
 import ar.net.edufmass.springrecipeapp.converters.RecipeToRecipeCommand;
 import ar.net.edufmass.springrecipeapp.repositories.RecipeRepository;
@@ -51,6 +52,26 @@ public class RecipeServiceImplTest {
     }
 
     @Test
+    public void getRecipeCommandById() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull(commandById, "Null recipe returned");
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
     public void getRecipes() throws Exception {
         Recipe recipe = new Recipe();
         HashSet recipesData = new HashSet();
@@ -61,7 +82,18 @@ public class RecipeServiceImplTest {
         Set<Recipe> recipes = recipeService.getRecipes();
 
         assertEquals(recipes.size(), 1);
-
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
+    }
+
+    @Test
+    public void deleteById() throws Exception {
+        //given
+        Long idToDelete = Long.valueOf(2L);
+        //when
+        recipeService.deleteById(idToDelete);
+        //no 'when', since method has void return type
+        //then
+        verify(recipeRepository, times(1)).deleteById(anyLong());
     }
 }
