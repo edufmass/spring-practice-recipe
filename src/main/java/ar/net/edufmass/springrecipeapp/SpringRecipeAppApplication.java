136af96bc891a017a159e9d23b0f8a77ff18aff0
edufmass <git@edufmass.ar>
2024-06-21 21:16:21 -0300

    Update recipe fixed

--- src/main/java/ar/net/edufmass/springrecipeapp/SpringRecipeAppApplication.java
+++ src/main/java/ar/net/edufmass/springrecipeapp/SpringRecipeAppApplication.java
@@ -10,8 +10,8 @@ public class SpringRecipeAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRecipeAppApplication.class, args);
-		System.out.println("version: " + SpringVersion.getVersion());
-		System.out.println("spring boot version: " + SpringBootVersion.getVersion());
+		//System.out.println("version: " + SpringVersion.getVersion());
+		//System.out.println("spring boot version: " + SpringBootVersion.getVersion());
 	}
 
 }
