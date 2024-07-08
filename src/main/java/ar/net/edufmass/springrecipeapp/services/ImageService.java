26833487b42918f8b17623de1b7db2b7793bd920
edufmass <git@edufmass.ar>
2024-07-08 18:22:08 -0300

    Upload image controller and view

---
+++ src/main/java/ar/net/edufmass/springrecipeapp/services/ImageService.java
@@ -0,0 +1,8 @@
+package ar.net.edufmass.springrecipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
